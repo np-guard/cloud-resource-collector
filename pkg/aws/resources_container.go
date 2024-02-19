9ea62669c58a624158e730f809d3ae69ff2bc690
Ola Saadi <ola.saadi1@ibm.com>
2024-02-19 14:20:57 +0200

    fix

--- pkg/aws/resources_container.go
+++ pkg/aws/resources_container.go
@@ -14,6 +14,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	aws2 "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+
 	"github.com/np-guard/cloud-resource-collector/pkg/common"
 )
 
